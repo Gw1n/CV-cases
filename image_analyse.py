42992656fac99eaf0686da3906aff4545a645a67
Doxy <63775961+Gw1n@users.noreply.github.com>
2025-01-24 16:41:20 +0300

    Create image_analyse.py

---
+++ image_analyse.py
@@ -0,0 +1,40 @@
+import cv2
+import serial
+import time
+
+img = cv2.imread("C:/Users/mvore/rainbow.jpg")
+
+#img = cv2.resize(img, (3, 3))
+
+#cv2.imshow("Image", img)
+#print(img)
+#print(img.shape)
+
+#Creating a list of average values of the image colors
+lights = [0, 0, 0]
+for k in range(0, 3, 1):
+    sum = 0.0
+    for i in range(0, img.shape[0], 1):
+        for j in range(0, img.shape[1], 1):
+            sum = img[i][j][k] + sum
+    lights[k] += sum/(img.shape[0] * img.shape[1])
+print(lights)
+
+transferData = ''
+
+for color in lights:
+    transferData += str(color)
+    transferData += ' '
+
+print(transferData)
+ser = serial.Serial('COM3', 9600, timeout = 5)
+time.sleep(2)
+ser.flush()
+ser.write(str.encode(transferData))
+time.sleep(1)
+response = ser.readline()
+decoded_response = response.decode('utf-8')
+ser.close()
+print(decoded_response)
+
+cv2.waitKey(0)
